00c2ccf8cb0d83bf7b87a3f3a854f5b05ad084dc
khalifah121 <faizolaberuagba@gmail.com>
2025-06-30 10:21:12 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import LoginSignup from './Components/LoginSignup'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <LoginSignup/>
+      </div>
+    </>
+  )
+}
+
+export default App
+
+
